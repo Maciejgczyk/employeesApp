f0793e94f150374112667cb096deabaa49e0f26c
Maciej Grzegorczyk <macieku00@gmail.com>
2022-01-14 01:55:43 +0100

    feature/header & styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MainComponent} from "./components/main/main.component";
+import { MainComponent } from "./components/main/main.component";
 import { HeaderComponent } from './components/header/header.component';
 import { EmployeesContainerComponent } from './components/employees-container/employees-container.component';
 import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +26,11 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [
     HttpClientModule
