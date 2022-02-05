b5d680b8d01d920761c9c6e8cc1f818e032f9f53
Mateusz Tomczyk <mateusz@mateuszt.pl>
2022-02-06 00:38:07 +0100

    refactor/search companies

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from "@angular/material/dialog";
+import { MatDialog } from '@angular/material/dialog';
 import { EmployeesService } from 'src/app/services/employees.service';
 import { AddEmployeeComponent } from '../dialogs/add-employee/add-employee.component';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  search: FormControl = new FormControl('');
 
-  constructor(private dialog: MatDialog, private employeesService: EmployeesService) { }
+  constructor(
+    private dialog: MatDialog,
+    private employeesService: EmployeesService
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.search.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => this.employeesService.sendSearchValue(value));
+  }
 
   addEmployee(): void {
-    this.dialog.open(AddEmployeeComponent)
+    this.dialog.open(AddEmployeeComponent);
   }
-
-  searchValue(searchValue: string) {
-    this.employeesService.sendSearchValue(searchValue);
-  }
-
 }
