b197ab26ba5da4907d96404c4e8e06582e25ce6e
Mateusz Tomczyk <mateusz@mateuszt.pl>
2022-02-05 01:28:26 +0100

    refactor/concatMap - employees container

--- src/app/components/employees-container/employees-container.component.ts
+++ src/app/components/employees-container/employees-container.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EmployeesService } from '../../services/employees.service';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { concatMap, takeUntil } from 'rxjs/operators';
 import { IEmployee } from 'src/app/interfaces/employee.model';
 
 @Component({
@@ -13,23 +13,22 @@ export class EmployeesContainerComponent implements OnInit, OnDestroy {
   destroyComponent$: Subject<boolean> = new Subject<boolean>();
   allEmployees: IEmployee[];
 
-  constructor(private employeesService: EmployeesService) { }
+  constructor(private employeesService: EmployeesService) {}
 
   ngOnInit(): void {
     this.getEmployees();
-    this.employeesService
-      .reloadEmployees$.subscribe(() => this.getEmployees());
+    this.employeesService.reloadEmployees$.subscribe(() => this.getEmployees());
 
     this.employeesService.searchValue$
-      .subscribe(value => this.employeesService.searchEmployees(value)
-        .subscribe(el => this.allEmployees = el));
+      .pipe(concatMap((value) => this.employeesService.searchEmployees(value)))
+      .subscribe((el) => (this.allEmployees = el));
   }
 
   getEmployees(): void {
     this.employeesService
       .getEmployees()
       .pipe(takeUntil(this.destroyComponent$))
-      .subscribe(items => this.allEmployees = items);
+      .subscribe((items) => (this.allEmployees = items));
   }
 
   ngOnDestroy(): void {
